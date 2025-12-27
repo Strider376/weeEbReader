70639d2cacfea7c88639d7bca5aa115d65b2da78
Strider 376 <noah.lewis0307@gmail.com>
2025-12-27 11:00:09 -0600

    update to test to use functions

--- GPIO_TEST.py
+++ GPIO_TEST.py
@@ -1,5 +1,23 @@
 from gpiozero import Button
+import signal
 
+def on_forward():
+    print("forward")
+
+def on_back():
+    print("backward")
+
+def on_up():
+    print("up")
+
+def on_down():
+    print("down")
+
+def on_select():
+    print("select")
+
+def on_menu():
+    print("menu")
 
 try:
     forward_button = Button(5)
@@ -8,12 +26,16 @@ try:
     up_button = Button(27)
     down_button = Button(3)
     menu_button = Button(4)
-except:
-    pass
-
-forward_button.when_activated = print("forward")
-back_button.when_activated = print("backward")
-up_button.when_activated = print("up")
-down_button.when_activated = print("down")
-select_button.when_activated = print("select")
-menu_button.when_activated = print("menu")
+    
+    forward_button.when_activated = on_forward
+    back_button.when_activated = on_back
+    up_button.when_activated = on_up
+    down_button.when_activated = on_down
+    select_button.when_activated = on_select
+    menu_button.when_activated = on_menu
+    
+    print("Button test running. Press Ctrl+C to exit.")
+    signal.pause()  # Wait forever until Ctrl+C
+    
+except KeyboardInterrupt:
+    print("\nExiting...")
